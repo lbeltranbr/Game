762247d36b01181ffbf89d7c86b02e50ec695e5c
lbeltranbr <57176574+lbeltranbr@users.noreply.github.com>
2021-02-18 20:19:45 +0000

    sprites implemented

--- Assets/Scripts/SimpleMovement.cs
+++ Assets/Scripts/SimpleMovement.cs
@@ -13,8 +13,9 @@ public class SimpleMovement : MonoBehaviour
         animator.SetFloat("Horizontal",Input.GetAxis("Horizontal"));
         animator.SetFloat("Vertical",Input.GetAxis("Vertical"));
         Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
-        Vector3 vertical = new Vector3( 0.0f, Input.GetAxis("Vertical"), Input.GetAxis("Vertical"));
-        transform.position = transform.position + (horizontal+vertical) * Time.deltaTime * speed;
+        /*Vector3 vertical = new Vector3( 0.0f, Input.GetAxis("Vertical"), Input.GetAxis("Vertical"));
+        transform.position = transform.position + (horizontal+vertical) * Time.deltaTime * speed;*/
+        transform.position = transform.position + horizontal * Time.deltaTime * speed;
 
         if(Input.GetKeyDown(KeyCode.Space))
             animator.SetTrigger("Jump");
