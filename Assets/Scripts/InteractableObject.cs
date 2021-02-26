f3e6a02149b69a43493ae3c789cf7cd3a69b28ff
lbeltranbr <57176574+lbeltranbr@users.noreply.github.com>
2021-02-26 16:26:04 +0000

    jump, colliders

---
+++ Assets/Scripts/InteractableObject.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject player;
+    public GameObject sprite;
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = ComputeDistance();
+
+        if (distance < 4)
+            sprite.SetActive(true);
+        else
+            sprite.SetActive(false);
+
+    }
+
+    private float ComputeDistance()
+    {
+        float dist = Vector3.Distance(this.gameObject.transform.position, player.transform.position);
+        return dist;
+    }
+}
