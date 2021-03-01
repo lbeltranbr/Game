b58689b156250c8d87b8fec38f9b444821809a27
lbeltranbr <57176574+lbeltranbr@users.noreply.github.com>
2021-03-01 01:27:48 +0000

    interaction

--- Assets/Scripts/turnOnLight.cs
+++ Assets/Scripts/turnOnLight.cs
@@ -2,40 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InteractableObject : MonoBehaviour
+public class turnOnLight : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public GameObject player;
-    public GameObject sprite_text;
     public GameObject light;
+    //public GameObject sprite_text;
 
+    //private bool on = false;
 
     // Update is called once per frame
     void Update()
     {
-        float distance = ComputeDistance();
+        /*float distance = ComputeDistance();
 
         if (distance < 2)
             sprite_text.SetActive(true);
         else
             sprite_text.SetActive(false);
 
-
         if (sprite_text.gameObject.activeSelf)
         {
             if (Input.GetKeyDown(KeyCode.E))
             {
-                light.SetActive(true);
-                this.gameObject.SetActive(false);
+                on = true;
             }
 
-        }
-
+        }*/
     }
-
-    private float ComputeDistance()
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        //Debug.Log("OnCollisionEnter2D");
+        light.SetActive(true);
+    }
+    /*private float ComputeDistance()
     {
         float dist = Vector3.Distance(this.gameObject.transform.position, player.transform.position);
         return dist;
-    }
+    }*/
 }
