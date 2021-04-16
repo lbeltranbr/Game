4b06750f08eb50a5b2719ed1c5c2244abce72e34
lbeltranbr <57176574+lbeltranbr@users.noreply.github.com>
2021-04-16 18:46:54 +0100

    cave update

---
+++ Assets/Scripts/Cave/Cracks.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cracks : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject player;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Animator anim = player.GetComponent<Animator>();
+            if (anim.GetCurrentAnimatorStateInfo(0).IsName("fade_in_out"))
+            {
+                Debug.Log("walking");
+            }
+
+        }
+
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Animator anim = player.GetComponent<Animator>();
+            if (anim.GetCurrentAnimatorStateInfo(0).IsName("fade_in_out"))
+            {
+                Debug.Log("walking");
+            }
+            else
+                anim.SetTrigger("fall");
+
+        }
+    }
+}
--- Assets/Scripts/SimpleMovement.cs
+++ Assets/Scripts/SimpleMovement.cs
@@ -8,21 +8,10 @@ public class SimpleMovement : MonoBehaviour
     public SpriteRenderer spriteRenderer;
     public Transform GroundCheck;
     public float speed;
-    public float JumpForce;
-
-    private float GroundRad = .1f;
-    private bool m_Grounded = true;
-    private bool jump = false;
-    private Rigidbody2D m_Rigidbody2D;
-    private Vector3 vertical = new Vector3(0.0f, 0.0f, 0.0f);
-
-    private const float GRAVITY = 10f;
 
 
-    private void Start()
-    {
-        m_Rigidbody2D = GetComponent<Rigidbody2D>();
-    }
+  
+
     // Update is called once per frame
     void Update()
     {
